cc3853357ea88c4408909745f0b342ba1177bd5e
Lexx <opavlyuk@gmail.com>
2018-10-29 20:56:00 +0200

    PreMENU

--- src/site/js/main/typewriter.js
+++ src/site/js/main/typewriter.js
@@ -1,16 +1,17 @@
-var i = 0;
-// var txt = ':КРАПКА';
-var txt = 'Проникливий дизайн...';
-var speed = 100;
+$(function() {
 
+    $('#gaslo').t({
+        delay: 2,
+        speed: 75,
+        // beep: true,
+        // blink: 300,
+        caret: '<span id="caret">|</span>',
+        fin:function(elm){
+        	$('#caret').fadeOut('5000');
+        	$('#name > span').addClass('blink');
+        	$('.scroll-btm').addClass('appear');
+        }
 
-function typeWriter() {
+    });
 
-  if (i < txt.length) {
-    $('#gaslo').append(txt.charAt(i)) ;
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-typeWriter();
+});
