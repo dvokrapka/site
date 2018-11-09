23f089251974411641bac0a77327d7fc72cc09bb
Lexx <opavlyuk@gmail.com>
2018-11-09 21:30:53 +0200

    JS Frontend Templater Updated

--- assets/js/templater.js
+++ assets/js/templater.js
@@ -1,60 +1,18 @@
 // Load & append element via XHTTP Request
-function xhttpLoad(url, selector, addType) {
+function insertTpl(tpl) {
 
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
 
-        var el = document.querySelector(selector);
-
         if (this.readyState == 4 && this.status == 200) {
 
-            // Append
-            if (addType === 'append') {
-                el.innerHTML += this.responseText;
-            }
-
-            // Prepend
-            if (addType === 'prepend') {
-                el.innerHTML = this.responseText + el.innerHTML;
-            }
+            var scr = document.querySelector('[data-tpl="' + tpl + '"]');
+            scr.insertAdjacentHTML('afterend', this.responseText);
+            scr.parentNode.removeChild(scr);
         }
     };
 
-    xhttp.open("GET", url + "?t=" + Math.random(), true);
+    xhttp.open("GET", 'assets/tpl/layout/' + tpl + '.tpl', true);
     xhttp.send();
 }
-
-// Templater
-function templateHelper() {
-
-    // Templates list
-    let layout = 'assets/tpl/layout/',
-        tpl = {
-            'favicon': {
-                'url': layout + 'meta/favicon.tpl',
-                'selector': 'head',
-                'addType': 'append'
-            },
-
-            'header': {
-                'url': layout + 'header.tpl',
-                'selector': 'body',
-                'addType': 'prepend'
-            },
-
-            'footer': {
-                'url': layout + 'footer.tpl',
-                'selector': 'body',
-                'addType': 'append'
-            },
-        };
-
-    let el = Object.values(tpl);
-
-        for (let i = 0, l = el.length; i < l; i++) {
-            xhttpLoad(el[i].url, el[i].selector, el[i].addType);
-        }
-}
-
-templateHelper();
