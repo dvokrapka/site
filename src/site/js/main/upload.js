078028c9b54b772ced62c648a9f557a033aa539d
Lexx <opavlyuk@gmail.com>
2018-11-19 12:51:38 +0200

    Brief upload preview

--- src/site/js/main/upload.js
+++ src/site/js/main/upload.js
@@ -1,16 +1,21 @@
 $(function() {
-    $('input[type=file]').on('change', function() {
+
+    var $input = $('input[type=file]');
+
+    $input.on('change', function() {
 
         if (this.files && this.files[0]) {
+
+            $('.upload-preview').remove();
 
             var up = this.files,
                 l = up.length;
 
             for (var i = 0; i < l; i++) {
 
-                console.log(up[i].name);
+                var $span = $('<span />').html(up[i].name).addClass('upload-preview');
+                $input.closest('.dv-file-upload').append($span);
             }
-
         }
     });
 });
